d7d38d52395be62addcc386b648f85a990418562
rohit-shete92 <rohitshete7018@gmail.com>
2026-03-14 17:54:58 +0530

    Add Main class with pattern printing logic

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Main {
+    public static void main(String[] args) {
+        int i,j;
+        for(i=1;i<=6;i++){
+            for(j=1;j<=i;j++){
+                if((i+j)%2==0){
+                    System.out.print("1");
+                }else{
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
